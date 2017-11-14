d263681b05d1f189b89aeacae88a8169fbe4bbe2
Daragh Behan <C00195564@itcarlow.ie>
2017-11-14 18:41:22 +0000

    adding lab 3 to pull on another computer

---
+++ Moving.Dot.SDL.Dot - Copy/Del/Del/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <SDL_ttf.h>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
